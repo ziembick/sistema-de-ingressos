9af4535ec91cfdb08d09199db592c48ecca6a1a5
Paulo Ziembick <pauloziembick@gmail.com>
2024-07-04 00:57:23 -0300

    teste

--- src/app/event/[eventId]/spots-layout/page.tsx
+++ src/app/event/[eventId]/spots-layout/page.tsx
@@ -8,23 +8,27 @@ import { cookies } from "next/headers";
 import { EventImage } from "@/app/components/EventImage";
 import { EventModel, SpotModel } from "@/models";
 
-
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export async function getSpots(eventId: string): Promise<{
   event: EventModel;
   spots: SpotModel[];
 }> {
-  const response = await fetch(
-    `${API_BASE_URL}/api/events/${eventId}/spots`,
-    {
-      cache: "no-store",
-      next: {
-        tags: [`events/${eventId}`]
-      }
-    }
-  );
+  const response = await fetch(`${API_BASE_URL}/api/events/${eventId}/spots`, {
+    cache: "no-store",
+    next: {
+      tags: [`events/${eventId}`],
+    },
+  });
 
-  return response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+  return {
+    event: data.event,
+    spots: data.spots,
+  };
 }
 
 export default async function SpotsLayoutPage({
@@ -32,151 +36,146 @@ export default async function SpotsLayoutPage({
 }: {
   params: { eventId: string };
 }) {
-  const { event, spots } = await getSpots(params.eventId);
-  //[a, a, a, b, b, c, d]
-  const rowLetters = spots.map((spot) => spot.name[0]);
+  try {
+    const { event, spots } = await getSpots(params.eventId);
 
-  //[a, b, c, d]
-  const uniqueRows = rowLetters.filter(
-    (row, index) => rowLetters.indexOf(row) === index
-  );
+    const rowLetters = spots.map((spot) => spot.name[0]);
+    const uniqueRows = rowLetters.filter(
+      (row, index) => rowLetters.indexOf(row) === index
+    );
 
-  const spotGroupedByRow = uniqueRows.map((row) => {
-    return {
-      row,
-      spots: [
-        ...spots
-          .filter((spot) => spot.name[0] == row)
+    const spotGroupedByRow = uniqueRows.map((row) => {
+      return {
+        row,
+        spots: spots
+          .filter((spot) => spot.name[0] === row)
           .sort((a, b) => {
             const aNumber = parseInt(a.name.slice(1));
-            const bNumber = parseInt(b.name.slice(2));
-
-            if (aNumber < bNumber) {
-              return -1;
-            }
-
-            if (aNumber > bNumber) {
-              return 1;
-            }
-
-            return 0;
+            const bNumber = parseInt(b.name.slice(1));
+            return aNumber - bNumber;
           }),
-      ],
-    };
-  });
+      };
+    });
 
-  const cookieStore = cookies();
-  const selectedSpots = JSON.parse(cookieStore.get("spots")?.value  || "[]");
+    const cookieStore = cookies();
+    const selectedSpots = JSON.parse(cookieStore.get("spots")?.value || "[]");
 
-  let totalPrice = selectedSpots.length * event.price
-  const ticketKind = cookieStore.get('ticketKind')?.value || "full"
+    let totalPrice = selectedSpots.length * event.price;
+    const ticketKind = cookieStore.get("ticketKind")?.value || "full";
 
-  if(ticketKind === 'half') {
-    totalPrice = totalPrice / 2
-  }
-  const formattedTotalPrice = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  }).format(totalPrice);
+    if (ticketKind === "half") {
+      totalPrice = totalPrice / 2;
+    }
 
+    const formattedTotalPrice = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(totalPrice);
 
-  return (
-    <main className="mt-10">
-      <div className="flex w-[1176px] max-w-full flex-row flex-wrap justify-center gap-x-8 rounded-2xl bg-secondary p-4 md:justify-normal">
-        <EventImage src={event.image_url} alt={event.name} />
-        <div className="flex max-w-full flex-col gap-y-6">
-          <div className="flex flex-col gap-y-2 ">
-            <p className="text-sm font-semibold uppercase text-subtitle">
-              {/* SÁB, 11/05/2024 - 20h00 */}
-              {new Date(event.date).toLocaleDateString("pt-BR", {
-                weekday: "long",
-                day: "2-digit",
-                month: "2-digit",
-                year: "numeric",
-              })}
-            </p>
-            <p className="text-2xl font-semibold">{event.name}</p>
-            <p className="font-normal">{event.location}</p>
-          </div>
-          <div className="flex h-[128px] flex-wrap justify-between gap-y-5 gap-x-3">
-            <div className="flex flex-col gap-y-2">
-              <p className="font-semibold">Organizador</p>
-              <p className="text-sm font-normal">{event.organization}</p>
+    return (
+      <main className="mt-10">
+        <div className="flex w-[1176px] max-w-full flex-row flex-wrap justify-center gap-x-8 rounded-2xl bg-secondary p-4 md:justify-normal">
+          <EventImage src={event.image_url} alt={event.name} />
+          <div className="flex max-w-full flex-col gap-y-6">
+            <div className="flex flex-col gap-y-2 ">
+              <p className="text-sm font-semibold uppercase text-subtitle">
+                {/* SÁB, 11/05/2024 - 20h00 */}
+                {new Date(event.date).toLocaleDateString("pt-BR", {
+                  weekday: "long",
+                  day: "2-digit",
+                  month: "2-digit",
+                  year: "numeric",
+                })}
+              </p>
+              <p className="text-2xl font-semibold">{event.name}</p>
+              <p className="font-normal">{event.location}</p>
             </div>
-            <div className="flex flex-col gap-y-2">
-              <p className="font-semibold">Classificação</p>
-              <p className="text-sm font-normal">{event.rating}</p>
+            <div className="flex h-[128px] flex-wrap justify-between gap-y-5 gap-x-3">
+              <div className="flex flex-col gap-y-2">
+                <p className="font-semibold">Organizador</p>
+                <p className="text-sm font-normal">{event.organization}</p>
+              </div>
+              <div className="flex flex-col gap-y-2">
+                <p className="font-semibold">Classificação</p>
+                <p className="text-sm font-normal">{event.rating}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <Title className="mt-10">Escolha seu lugar</Title>
-      <div className="mt-6 flex flex-wrap justify-between">
-        <div className=" mb-4 flex w-full max-w-[650px] flex-col gap-y-8 rounded-2xl bg-secondary p-6">
-          <div className="rounded-2xl bg-bar py-4 text-center text-[20px] font-bold uppercase text-white">
-            Palco
-          </div>
-          <div className="md:w-full md:justify-normal">
-            {spotGroupedByRow.map((row) => {
-              return (
-                <div
-                  key={row.row}
-                  className="flex flex-row gap-3 items-center mb-3"
-                >
-                  <div className="w-4">{row.row}</div>
-                  <div className="ml-2 flex flex-row">
-                    {row.spots.map((spot) => {
-                      return (
-                        <SpotSeat
-                          key={spot.name}
-                          spotId={spot.name}
-                          spotLabel={spot.name.slice(1)}
-                          selected={selectedSpots.includes(spot.name)}
-                          disabled={spot.status === 'sold'}
-                          eventId={event.id}
-                        />
-                      );
-                    })}
+        <Title className="mt-10">Escolha seu lugar</Title>
+        <div className="mt-6 flex flex-wrap justify-between">
+          <div className=" mb-4 flex w-full max-w-[650px] flex-col gap-y-8 rounded-2xl bg-secondary p-6">
+            <div className="rounded-2xl bg-bar py-4 text-center text-[20px] font-bold uppercase text-white">
+              Palco
+            </div>
+            <div className="md:w-full md:justify-normal">
+              {spotGroupedByRow.map((row) => {
+                return (
+                  <div
+                    key={row.row}
+                    className="flex flex-row gap-3 items-center mb-3"
+                  >
+                    <div className="w-4">{row.row}</div>
+                    <div className="ml-2 flex flex-row">
+                      {row.spots.map((spot) => {
+                        return (
+                          <SpotSeat
+                            key={spot.name}
+                            spotId={spot.name}
+                            spotLabel={spot.name.slice(1)}
+                            selected={selectedSpots.includes(spot.name)}
+                            disabled={spot.status === "sold"}
+                            eventId={event.id}
+                          />
+                        );
+                      })}
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
+            <div className="flex w-full flex-row justify-around">
+              <div className=" flex flex-row items-center">
+                <span className="mr-1 block h-4 w-4 rounded-full bg-[#00A96E]" />
+                Disponível
+              </div>
+              <div className=" flex flex-row items-center">
+                <span className="mr-1 block h-4 w-4 rounded-full bg-[#A6ADBB]" />
+                Ocupado
+              </div>
+              <div className=" flex flex-row items-center">
+                <span className="mr-1 block h-4 w-4  rounded-full bg-[#7480FF]" />
+                Selecionado
+              </div>
+            </div>
           </div>
-          <div className="flex w-full flex-row justify-around">
-            <div className=" flex flex-row items-center">
-              <span className="mr-1 block h-4 w-4 rounded-full bg-[#00A96E]" />
-              Disponível
+          <div className="flex w-full max-w-[478px] flex-col gap-y-6 rounded-2xl bg-secondary px-4 py-6">
+            <h1 className="text-[20px] font-semibold">
+              Confira os valores do evento
+            </h1>
+            <p>
+              Inteira: {"R$ 100,00"} <br />
+              Meia-entrada: {`R$ 50,00`}
+            </p>
+            <div className="flex flex-col">
+              <TicketKindSelect
+                defaultValue={ticketKind as any}
+                price={event.price}
+              />
             </div>
-            <div className=" flex flex-row items-center">
-              <span className="mr-1 block h-4 w-4 rounded-full bg-[#A6ADBB]" />
-              Ocupado
-            </div>
-            <div className=" flex flex-row items-center">
-              <span className="mr-1 block h-4 w-4  rounded-full bg-[#7480FF]" />
-              Selecionado
-            </div>
+            <div>Total: {formattedTotalPrice}</div>
+            <Link
+              href="/checkout"
+              className="rounded-lg bg-btn-primary py-4 text-sm font-semibold uppercase text-btn-primary text-center hover:bg-[#fff]"
+            >
+              Ir para pagamento
+            </Link>
           </div>
         </div>
-        <div className="flex w-full max-w-[478px] flex-col gap-y-6 rounded-2xl bg-secondary px-4 py-6">
-          <h1 className="text-[20px] font-semibold">
-            Confira os valores do evento
-          </h1>
-          <p>
-            Inteira: {"R$ 100,00"} <br />
-            Meia-entrada: {`R$ 50,00`}
-          </p>
-          <div className="flex flex-col">
-            <TicketKindSelect defaultValue={ticketKind as any} price={event.price}/>
-          </div>
-          <div>Total: {formattedTotalPrice}</div>
-          <Link
-            href="/checkout"
-            className="rounded-lg bg-btn-primary py-4 text-sm font-semibold uppercase text-btn-primary text-center hover:bg-[#fff]"
-          >
-            Ir para pagamento
-          </Link>
-        </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  } catch (error) {
+    console.error("Error fetching or processing data:", error);
+    throw new Error("Server-side rendering error");
+  }
 }
