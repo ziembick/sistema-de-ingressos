c9df406e0bf2c9a092d54c6444638143835e4993
Paulo Ziembick <pauloziembick@gmail.com>
2024-07-04 00:42:27 -0300

    spots

--- src/app/event/[eventId]/spots-layout/page.tsx
+++ src/app/event/[eventId]/spots-layout/page.tsx
@@ -24,6 +24,10 @@ export async function getSpots(eventId: string): Promise<{
     }
   );
 
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
   return response.json();
 }
 
